0fd091671ee9568763c1715df1d0795d298c0b31
namcigam <slideyourride@gmail.com>
2017-03-21 12:27:03 -0500

    Party Rock working after set height constrait on stackview parent veiw

--- PartyRockApp/PartyRockApp/VideoVC.swift
+++ PartyRockApp/PartyRockApp/VideoVC.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class VideoVC: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
+    @IBOutlet weak var titleLBL: UILabel!
+    private var _partyRock: PartyRock!
+    
+    var partyRock: PartyRock {
+        get {
+            return _partyRock
+        } set {
+            _partyRock = newValue
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        titleLBL.text = partyRock.videoTitle
+        webView.loadHTMLString(partyRock.videoURL, baseURL: <#T##URL?#>)
     }
 
     override func didReceiveMemoryWarning() {
